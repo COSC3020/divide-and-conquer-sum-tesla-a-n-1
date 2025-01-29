a943765b604c939f50a7ff4fd085776c64384815
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-01-29 15:19:41 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,22 @@
 function divideAndConquerSum(a) {
-    return a;
+    if (a.length === 0) {
+        return 0; // Return 0 for an empty array
+    } else if (a.length === 1) {
+        return a[0]; // Return the single element
+    } else if (a.length === 2) {
+        // Sum two elements directly because we're thirding the lists
+        return a[0] + a[1];
+    }
+
+    const split = Math.floor(a.length / 3);
+    const left = divideAndConquerSum(a.slice(0, split)); //first third
+    const mid = divideAndConquerSum(a.slice(split, split * 2)); //middle third
+    const right = divideAndConquerSum(a.slice(split * 2));  //last third
+
+    // Sum the results of left, mid, and right
+    return left + mid + right;
 }
+/*
+const array = [38, 27, 43, 3, 9, 82, 10, 0];
+const summ = divideAndConquerSum(array);
+console.log("Sum:", summ); */
